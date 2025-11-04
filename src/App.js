c4f8f6d38c87437f5f4940517a411e5f7ff3c1b1
PrasannaKumar-3065 <prasanna.tamilan2002@gmail.com>
2025-11-05 01:09:46 +0530

    z

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Component } from 'react';
 import logo from './logo.svg';
 import ChatBot from './components/ChatBot'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <ChatBot />
+      <Routes>
+      <Route path="/" element={<ChatBot />} />
+      </Routes>
   );
 }
 
