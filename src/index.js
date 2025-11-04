08f93c4aa4b9456acecd6925b55df4b8623ea2fe
PrasannaKumar-3065 <prasanna.tamilan2002@gmail.com>
2025-11-05 00:57:17 +0530

    s

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
